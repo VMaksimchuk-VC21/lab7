63f47f5dfaf2e01cbada1ab1ea895a72cd35b2d4
Валерия Максимчук <VMaksimchuk-VC21@stud.sfu-kras.ru>
2024-01-15 01:21:53 +0700

    class

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
 class TransportCompany:
-    def __init__(self):
-        return
+    def __init__(self, name):
+        self.__name = name
+        self.__vehicles = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def vehicles(self):
+        return self.__vehicles
 
     def __str__(self):
-        return
+        return f"Фирма: {self.name}"
+
+    def add_vehicle(self, vehicle):
+        self.__vehicles.append(vehicle)
 
 
 class Vehicle:
-    def __init__(self):
-        return
+    def __init__(self, model, company):
+        self.__model = model
+        self.__company = company
+        self.__drivers = []
+
+    @property
+    def model(self):
+        return self.__model
+
+    @property
+    def company(self):
+        return self.__company
+
+    @property
+    def drivers(self):
+        return self.__drivers
 
     def __str__(self):
-        return
+        return f"Транспорт: {self.model} ({self.company.name})"
+
+    def add_driver(self, driver):
+        self.__drivers.append(driver)
 
 
 class Driver:
-    def __init__(self):
-        return
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
 
     def __str__(self):
-        return
+        return f"Водитель: {self.name}"
 
