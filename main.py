2fb73b45313a90bd1f10133518ebe74922ef1538
Валерия Максимчук <VMaksimchuk-VC21@stud.sfu-kras.ru>
2024-01-15 01:29:24 +0700

    menu

--- main.py
+++ main.py
@@ -70,3 +70,43 @@ def create_driver():
     name = input("Введите имя водителя: ")
     return Driver(name)
 
+
+def menu():
+    company = None
+    vehicle = None
+    driver = None
+
+    while True:
+        print("\nМеню:")
+        print("1. Создать фирму")
+        print("2. Создать транспорт")
+        print("3. Создать водителя")
+        print("4. Вывести информацию о фирме")
+        print("5. Вывести информацию о транспорт")
+        print("6. Вывести информацию о водителе")
+        print("7. Выход из программы")
+
+        point = input("Выберите пункт меню: ")
+
+        if point == "1":
+            pass
+        elif point == "2":
+            pass
+        elif point == "3":
+            pass
+        elif point == "4":
+            pass
+        elif point == "5":
+            pass
+        elif point == "6":
+            pass
+        elif point == "7":
+            pass
+        else:
+            pass
+
+
+if __name__ == "__main__":
+    menu()
+
+    
